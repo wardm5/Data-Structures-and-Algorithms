a6ae3e893c2d93ad75e45d740fa25a5a2ceb46d0
Misha Ward <wardm5@uw.edu>
2019-07-19 10:46:46 -0700

    added memoization methods for fib and fact

--- Algorithms/DynamicProgramming.java
+++ Algorithms/DynamicProgramming.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
     This file is focused on solving Dynamic Programming questions for interview situations and fun practice :D
     Note:   methods will have the problem, underscore, and either TAB or MEMO denoting what type of dynamic programming
@@ -10,7 +12,7 @@ public class DynamicProgramming {
         int[] arr = new int[n + 1];
         return fibonacciHelper_TAB(n, arr);
     }
-    public int fibonacciHelper_TAB(int n, int[] arr) {
+    private int fibonacciHelper_TAB(int n, int[] arr) {
         if (n < 1)
             return 0;
         if (n == 1)
@@ -21,6 +23,20 @@ public class DynamicProgramming {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n];
+    }
+    public int fibonacci_MEMO(int n) {
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, -1);
+        return fibonacciHelper_MEMO(n, arr);
+    }
+    private int fibonacciHelper_MEMO(int n, int[] arr) {
+        if (n < 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (arr[n] != -1)
+            return arr[n];
+        return (arr[n] = (fibonacciHelper_MEMO(n-1, arr) + fibonacciHelper_MEMO(n-2, arr)));
     }
 
     public int factorial_TAB(int n) {
@@ -34,6 +50,19 @@ public class DynamicProgramming {
             arr[i] = arr[i - 1] * i;
         }
         return arr[n];
+    }
+    public int factorial_MEMO(int n) {
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, -1);
+        return factorialHelper_MEMO(n, arr);
+
+    }
+    private int factorialHelper_MEMO(int n, int[] arr) {
+        if (n == 0)
+            return 1;
+        if (arr[n] != -1)
+            return arr[n];
+        return (arr[n] = n * factorialHelper_MEMO(n - 1, arr));
     }
     /*
         We are given two strings: string S of length n, and string T of length m. Our goal is to produce their
--- Algorithms/main.java
+++ Algorithms/main.java
@@ -1,23 +1,39 @@
+import java.util.*;
 public class main {
     private static DynamicProgramming dp = new DynamicProgramming();
     public static void main(String[] args) {
+        //    NOTE: 0 = TAB, 1 = MEMO
         fib("fibonacci_TAB");
         fact("factorial_TAB");
     }
     private static void print(String str) {
-        System.out.println();
         System.out.println(str);
     }
+    private static void newLine(){
+        System.out.println();
+    }
     private static void fib(String str) {
-        print(str.toUpperCase() + " TESTS");
+        newLine();
+        print("******** " + str.toUpperCase() + " TESTS ********");
+        print("Tabulation");
         for(int i = 0; i <= 10; i++) {
             System.out.println(i + "  " + dp.fibonacci_TAB(i));
         }
+        print("Memoization");
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(i + "  " + dp.fibonacci_MEMO(i));
+        }
     }
     private static void fact(String str) {
-        print(str.toUpperCase() + " TESTS");
+        newLine();
+        print("******** " + str.toUpperCase() + " TESTS ********");
+        print("Tabulation");
         for(int i = 0; i <= 10; i++) {
             System.out.println(i + "  " + dp.factorial_TAB(i));
+        }
+        print("Memoization");
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(i + "  " + dp.factorial_MEMO(i));
         }
     }
 

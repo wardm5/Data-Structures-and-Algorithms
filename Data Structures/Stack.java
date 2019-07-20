7e1da4a0bddda0ce47dd2f79c69aedea276538c1
Misha Ward <wardm5@uw.edu>
2019-07-19 18:48:13 -0700

    updating statck data structure, added constructor, push, working on pop and peek

--- Data Structures/Stack.java
+++ Data Structures/Stack.java
@@ -1,4 +1,14 @@
-public class Stack {
-    
+public class Stack<T> {
+    private Node<T> head;
+    private int count;
+    public Stack() {
+        this.head = null;
+        this.count = 0;
+    }
+    public Stack(T data) {
+        this.head = new Node(data);
+        this.count = 1;
+    }
+    public boolean add(T data)
 
 }

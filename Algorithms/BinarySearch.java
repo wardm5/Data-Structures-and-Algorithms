5d5f800c8016ecc7f87a57a240df8364c5f14e8b
Misha Ward <wardm5@uw.edu>
2019-10-16 22:02:04 -0700

    added second binary search which has only two values and finding where the change occurs

--- Algorithms/BinarySearch.java
+++ Algorithms/BinarySearch.java
@@ -2,7 +2,9 @@ public class BinarySearch{
      public static void main(String []args){
         System.out.println("Hello World");
         int[] arr = new int[] {1,3,4,5,6,7,8,9};
-        System.out.println(binarySearch(arr, -1)); // 1
+        System.out.println(binarySearch(arr, -1));
+        int[] arr2 = new int[] {1,1,1,1,1,1,1,1};
+        System.out.println(binarySearch2(arr2));  // 5
      }
      public static int binarySearch(int[] arr, int target) {
          int left = 0;
@@ -12,6 +14,21 @@ public class BinarySearch{
             if (arr[mid] == target) {
                 return mid;
             } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+         }
+         return -1;
+     }
+     public static int binarySearch2(int[] arr) {
+         int left = 0;
+         int right = arr.length-1;
+         while (left < right) {
+            int mid = ((right - left) / 2) + left;
+            if (arr[mid] == 0 && arr[mid + 1] == 1)
+                return mid;
+            else if (arr[mid] == 1) {
                 right = mid - 1;
             } else {
                 left = mid + 1;

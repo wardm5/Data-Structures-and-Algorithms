10235763d65027b0f1cb3fceda183075cc3ab46e
Misha Ward <wardm5@uw.edu>
2019-12-30 21:27:11 -0600

    added size method for Queue method

--- Python/Data_Structures/Queue/Queue.py
+++ Python/Data_Structures/Queue/Queue.py
@@ -8,7 +8,7 @@ from Data_Structures.Node.Node import *
     def __init__(self, value):
         self.head = Node(value)
         self.tail =  self.head
-        self.count = 0
+        self.count = 1
 
     def add(self, value):
         if (self.head == None):
@@ -17,17 +17,20 @@ from Data_Structures.Node.Node import *
         else:
             tail.next =  Node(value)
             tail = tail.next
+        self.count = self.count + 1
         return True
 
     def remove(self):
         if (self.head != None):
             res = self.head.val
             self.head = self.head.next
+            self.count = self.count - 1
             return res
         else:
             return None
-    def size():
 
+    def size(self):
+        return self.count
 
     def contains():
         

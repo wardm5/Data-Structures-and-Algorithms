051300e7de197df8cc02859e3e719a1931bb859d
Misha Ward <wardm5@uw.edu>
2020-01-01 10:23:31 -0600

    added initialization/constructor method and push method for Stack DS

---
+++ Python/Data_Structures/Stack/Stack.py
@@ -0,0 +1,17 @@
+from Data_Structures.Node.Node import *
+
+class Stack():
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def push(self, value):
+        if (self.head == None):
+            self.head = Node(value)
+        else:
+            temp = Node(value)
+            temp.next = self.head
+            self.head = temp
+        return true
+
+    

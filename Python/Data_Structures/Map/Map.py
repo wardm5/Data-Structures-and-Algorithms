d73ac593400e33dffebcb66fce9226fe56c33272
Misha Ward <wardm5@uw.edu>
2020-01-02 16:44:10 -0600

    updated map method descriptions

--- Python/Data_Structures/Map/Map.py
+++ Python/Data_Structures/Map/Map.py
@@ -1,3 +1,4 @@
+# descriptions from https://docs.oracle.com/javase/8/docs/api/java/util/Map.html
 from Data_Structures.Entry.Entry import *
 
 class Map():
@@ -6,6 +7,7 @@ class Map():
         self.size = size
         self.count = 0
 
+    # Associates the specified value with the specified key in this map (optional operation).
     def put(self, key, value):
         hashVal = hash(key) % self.size
         if (self.array[hashVal] == None):
@@ -21,6 +23,7 @@ class Map():
             curr.next = Entry(key, value)
         self.count = self.count + 1
 
+    # Returns the value to which the specified key is mapped, or defaultValue if this map contains no mapping for the key.
     def getOrDefault(self, key, defaultVal):
         hashVal = hash(key) % self.size
         if (self.array[hashVal] == None):
@@ -34,6 +37,7 @@ class Map():
                     curr = curr.next
             return defaultVal
 
+    # Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
     def get(self, key):
         hashVal = hash(key) % self.size
         if (self.array[hashVal] == None):
@@ -73,8 +77,9 @@ class Map():
 
     # Removes the mapping for a key from this map if it is present (optional operation).
     def remove(self, key):
-        
+        print("remove")
 
+    # Removes all of the mappings from this map (optional operation).
     def clear(self):
         self.array.clear()
         self.array = [None] * self.size
